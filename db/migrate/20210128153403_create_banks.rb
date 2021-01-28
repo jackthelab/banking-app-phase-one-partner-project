fbfc669623e0357667d6cf57178f4b0504bce312
Jacob Hampton <jhampton792@gmail.com>
2021-01-28 10:37:20 -0600

    Updated Migrations for CreateUsers and CreateBanks

--- db/migrate/20210128153403_create_banks.rb
+++ db/migrate/20210128153403_create_banks.rb
@@ -1,4 +1,13 @@
 class CreateBanks < ActiveRecord::Migration[5.2]
   def change
+    create_table :banks do |t|
+      t.string :name
+      t.string :username
+      t.string :password
+      t.string :city
+      t.string :phone_number
+      t.string :website_url
+      t.timestamps
+    end
   end
 end
