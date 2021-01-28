22d597f9c6bb8066fadc855bde41301a2d310b5b
Jacob Hampton <jhampton792@gmail.com>
2021-01-28 10:47:09 -0600

    Updated CreateAccounts migration file

--- db/migrate/20210128153508_create_accounts.rb
+++ db/migrate/20210128153508_create_accounts.rb
@@ -1,7 +1,13 @@
 class CreateAccounts < ActiveRecord::Migration[5.2]
   def change
     create_table :accounts do |t|
-      #
+      t.string :type
+      t.float :amount
+      t.string :status
+      t.timestamps
     end
+
+    add_foreign_key :accounts, :users
+    add_foreign_key :accounts, :banks
   end
 end
