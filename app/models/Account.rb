2b411ae943dec16adf4a44639588f23f36fc8164
Jacob Hampton <jhampton792@gmail.com>
2021-01-28 21:17:57 -0600

    Added to Bank and Account classes

--- app/models/Account.rb
+++ app/models/Account.rb
@@ -1,2 +1,7 @@
 class Account < ActiveRecord::Base
+
+    def close(party)
+        self.status = "closed by #{party.class}: #{party.username}"
+    end
+
 end
--- app/models/Bank.rb
+++ app/models/Bank.rb
@@ -1,2 +1,35 @@
 class Bank < ActiveRecord::Base
+
+    def accounts
+        Accounts.where("bank_id = ?", self.id)
+    end
+
+    def open_accounts
+        self.accounts.where("status = ?", "open")
+    end
+
+    def closed_accounts
+        self.accounts.where("status IS NOT ?", "open")
+    end
+
+    def number_of_open_accounts
+        self.open_accounts.count
+    end
+
+    def number_of_customers
+        self.open_accounts.select { |account| account.user_id}.uniq.count
+    end
+
+    def total_value_of_all_accounts
+        self.open_accounts.sum(:amount)
+    end
+
+    def see_accounts_above(num)
+        self.open_accounts.where("amount > ?", num)
+    end
+
+    def close_account
+        # some_account.close(self)
+    end
+
 end
